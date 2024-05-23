8388edbb2176f1ed3a4fd52a2237f17979c336e1
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2024-05-24 00:20:03 +0300

    feat: add ratings page

--- app/rated/page.tsx
+++ app/rated/page.tsx
@@ -2,14 +2,16 @@
 
 import { Flex, Title } from "@mantine/core";
 import { useEffect, useState } from "react";
-import { getMoviesRatings } from "../lib/rating";
+import { getMoviesRatings } from "@/app/lib/rating";
 import { MoviesList } from "@/app/ui/movies/movies-list";
 import { Pagination } from "@/app/ui/movies/pagination";
 import cls from './rated.module.scss'
+import { Searchbar } from "@/app/ui/rated/searchbar";
 
 export default function Rated() {
     const [movies, setMovies] = useState<RatedMovie[]>([]);
     const [page, setPage] = useState<number>(1);
+    const [searchString, setSearchString] = useState<string>('');
 
     const moviesOnPage = 4;
 
@@ -18,11 +20,37 @@ export default function Rated() {
         if (movies) setMovies(movies);
     }, []);
 
+    const searchMovies = () => {
+        if (!searchString) {
+            const moviesRatings = getMoviesRatings();
+            console.log(moviesRatings);
+            if (moviesRatings) setMovies(moviesRatings);
+
+            return;
+        }
+
+        setMovies(
+            movies
+                .filter(m =>
+                    m.original_title
+                        .toLowerCase()
+                        .includes(
+                            searchString
+                                .toLowerCase()
+                        )
+                )
+        )
+    }
+
     return (
         <Flex direction='column' gap={40} className={cls.wrapper}>
-            <Flex>
+            <Flex align='center' justify='space-between'>
                 <Title order={1}>Rated movies</Title>
-                <></>
+                <Searchbar
+                    value={searchString}
+                    setValue={setSearchString}
+                    onButtonClick={searchMovies}
+                />
             </Flex>
             <MoviesList movies={movies.slice(moviesOnPage * (page - 1), moviesOnPage * page)} />
             <Pagination
