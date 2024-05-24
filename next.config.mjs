4027264b2293b590bf0bd3077703d08872747911
DonatArtihovich <117632852+DonatArtihovich@users.noreply.github.com>
2024-05-24 22:04:22 +0300

    fix: try to fix deploy

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ['image.tmdb.org'],
     },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'app')],
+  },
 };
 
 export default nextConfig;
